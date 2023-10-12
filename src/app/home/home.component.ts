2153631414175716c4dfbc783076191d087b2827
KieranMueller <kieran98mueller@gmail.com>
2023-10-12 12:55:45 -0500

    homepage nickname

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  hasName = false;
+  name = '';
+
+  ngOnInit() {
+    const name = localStorage.getItem('nickname');
+    if (name) {
+      this.name = name;
+      this.hasName = true;
+    }
+  }
+
+  setName() {
+    this.hasName = true;
+    localStorage.setItem('nickname', this.name);
+  }
+
+  editName() {
+    this.hasName = false;
+  }
+}
